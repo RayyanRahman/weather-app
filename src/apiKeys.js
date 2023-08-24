67c0cdcce8beb63bb571a86605ac77c22dd3543d
Rayyan Rahman <129946782+RayyanRahman@users.noreply.github.com>
2023-08-24 22:04:29 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,7 @@
+// Visit https://api.openweathermap.org & then signup to get API keys for free
+module.exports = {
+  key: "Your API KEY",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+// https://api.openweathermap.org/data/2.5/weather?lat=28.67&lon=77.22&units=metric&APPID=
